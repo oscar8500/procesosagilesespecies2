c8bb2cdfe3073f2966ca7220cc80cb4a7b17d4ba
Omar <omarcz27@gmail.com>
2017-02-06 19:19:35 -0500

    se adiciona la funcionalidad login

--- especies/urls.py
+++ especies/urls.py
@@ -4,7 +4,11 @@ from . import views
 
 urlpatterns = [
     url(r'^addUser/$', views.adicionar_usuario, name='addUser'),
+    url(r'^logout/$', views.logout_request, name='logout'),
+    url(r'^isLogged/$', views.islogged, name='isLogged'),
+    url(r'^login/$', views.login_request, name='login'),
 
     url(r'^listar/$', views.index_especies, name='listar'),
     url(r'^agregarUsuario/$', views.index_usuario, name='agregarUsuario'),
+    url(r'^ingresar/$', views.ingresar, name='ingresar'),
 ]
--- especies/views.py
+++ especies/views.py
@@ -1,10 +1,12 @@
 import json
 
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
 from django.core import serializers
+from django.http import JsonResponse
 
 # Create your views here.
 from especies.models import UserProfile
@@ -31,9 +33,45 @@ def adicionar_usuario(request):
     return HttpResponse(serializers.serialize("json", [user_model]))
 
 
+@csrf_exempt
+def login_request(request):
+    if request.method == 'POST':
+        jsonUser = json.loads(request.body)
+        username = jsonUser['username']
+        password = jsonUser['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            mensaje = 'ok'
+        else:
+            mensaje = 'Nombre de usuario o clave no valido'
+
+    return JsonResponse({"mensaje": mensaje})
+
+
+@csrf_exempt
+def logout_request(request):
+    logout(request)
+    return JsonResponse({"mensaje": 'ok'})
+
+
+@csrf_exempt
+def islogged(request):
+    if request.user.is_authenticated():
+        mensaje = 'ok'
+    else:
+        mensaje = 'no'
+
+    return JsonResponse({"mensaje": mensaje})
+
+
 def index_especies(request):
     return render(request, 'especies/index.html')
 
 
 def index_usuario(request):
     return render(request, 'especies/registro.html')
+
+
+def ingresar(request):
+    return render(request, 'especies/login.html')
