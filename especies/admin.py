3300bfec960c685d6d512f11565edb29fe3c5ad2
Daniel Esteban Puerta <daespuor@hotmail.com>
2017-02-07 00:40:37 -0500

    Modelos agregados a la administración

--- especies/admin.py
+++ especies/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Comment
+from .models import Specie
+from .models import UserProfile
+from .models import Category
+
 
 # Register your models here.
+admin.site.register(Comment)
+admin.site.register(Specie)
+admin.site.register(UserProfile)
+admin.site.register(Category)
