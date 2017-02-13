eea832cd01d81716078fc03116eae16c50a820a4
Omar <omarcz27@gmail.com>
2017-02-12 19:10:57 -0500

    ajustes

--- especies/urls.py
+++ especies/urls.py
@@ -3,13 +3,15 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # Peticiones Ajax o  JavaScript
     url(r'^addUser/$', views.adicionar_usuario, name='addUser'),
     url(r'^logout/$', views.logout_request, name='logout'),
     url(r'^isLogged/$', views.islogged, name='isLogged'),
     url(r'^login/$', views.login_request, name='login'),
     url(r'^fillCities/$', views.fillCities, name='fillCities'),
-    url(r'^$', views.index_especies, name='listar'),
-    url(r'^listar/$', views.index_especies, name='listar'),
+
+    # Peticiones a vistas
+    url(r'^$', views.index_especies, name=''),
     url(r'^agregarUsuario/$', views.index_usuario, name='agregarUsuario'),
     url(r'^ingresar/$', views.ingresar, name='ingresar'),
 ]
